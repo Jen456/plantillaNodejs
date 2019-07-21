d184f430dc548107dfd8afd85928fdf60df5196e
Jenny Alava <>
2019-07-21 13:16:10 -0500

    instalando dependencias para la plataforma

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +8,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .server.listen('8000', function() {
+    console.log("App is running on port 8000");
+});
